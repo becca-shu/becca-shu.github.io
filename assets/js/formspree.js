6942f879bcd603eeec1d29e1e7013c816fde32c0
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2017-03-04 19:23:52 -0500

    form with formspree and ajax

---
+++ assets/js/formspree.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const $form = $('#contact-form');
+
+  $form.on('submit', function (e) {
+    e.preventDefault();
+
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const message = $('#message').val();
+
+    $.ajax({
+      url:'https://formspree.io/tarotisforeveryone@gmail.com',
+      method:'POST',
+      data:{
+          name: name,
+          _replyto: email,
+           email: email,
+          message: message,
+          _subject: `message from ${name}`,
+      },
+      dataType:"json",
+      success:function() {
+          console.log('success');
+      }
+    });
+
+  });
+
+});
